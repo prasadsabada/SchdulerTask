10a6aa3b4cb332fc36fe22aaf20b1f79394ae4d1
prasadsabada <prasad.csr07@gmail.com>
2022-02-10 13:00:51 +0530

    added schedular task job

---
+++ src/main/java/com/example/schedulartask/SchedularJobTask.java
@@ -0,0 +1,14 @@
+package com.example.schedulartask;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+public class SchedularJobTask {
+	
+	@Scheduled(fixedRate = 500)
+	public void startTask() {
+		System.out.println("SchedularJobTask.startTask()............... job started");
+	}
+
+}
