a9f72b6ca0a30c5864da93e53fbea2b04ef26f83
prasadsabada <prasad.csr07@gmail.com>
2022-02-10 14:00:33 +0530

    added one more methoed in ctrl

--- src/main/java/com/example/controller/ScheduleController.java
+++ src/main/java/com/example/controller/ScheduleController.java
@@ -1,6 +1,9 @@
 package com.example.controller;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +12,11 @@ public class ScheduleController {
 	@GetMapping("/")
 	public String test() {
 		return "hello";
+		
+	}
+	@PostMapping("/")
+	public String postTest() {
+		return "hello world";
 		
 	}
 }
