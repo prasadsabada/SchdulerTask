629df385c838fae7d05f3c6056f3144ab6202dd2
prasadsabada <prasad.csr07@gmail.com>
2022-02-10 13:51:35 +0530

    added controller

--- src/main/java/com/example/schedulartask/SchedularJobTask.java
+++ src/main/java/com/example/schedulartask/SchedularJobTask.java
@@ -6,7 +6,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 @Configuration
 public class SchedularJobTask {
 	
-	@Scheduled(fixedRate = 500)
+	@Scheduled(fixedRate = 5000)
 	public void startTask() {
 		System.out.println("SchedularJobTask.startTask()............... job started");
 	}
